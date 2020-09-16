a98f9056ff09fd2211c0dcfd60eb902b9fd5b1d9
SzklevanMilan <szklevanm@gmail.com>
2020-09-16 08:29:28 +0200

    Add project files.

---
+++ Kiralynok/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    class Tabla
+    {
+        char[,] tomb= new char[8,8];
+        char Urescella;
+        int UresOszlopokSzama;
+        int UresSorokSzama;
+        public void Elhelyez()
+        { 
+        
+        }
+        public void FajlbaIr()
+        { 
+        
+        }
+        public void Megjelent()
+        { 
+        
+        }
+        public char Table()
+        { 
+        
+        }
+        public int UresOszlop()
+        { 
+        
+        }
+        public int UresSor()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
